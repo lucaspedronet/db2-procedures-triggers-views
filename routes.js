8229aa087e426f2059039155fb5774e41fca4066
lucaspedronet <lucaspedrolbg@gmail.com>
2018-11-12 20:11:28 -0300

    Criando função de cadastrar funcionários

--- routes.js
+++ routes.js
@@ -5,28 +5,30 @@ const procedure = require('./procedures/index')
 
 routes.use(bodyParser.urlencoded({ extended: false }));
 
-routes.get('/funcionario/:id', (req, res) => {
-  return res.send('Funcionou!')
+routes.get('/cadastrar/funcionario', (req, res) => {
+  res.render('cadastrarFuncionario')
 })
 
- routes.get('/funcionario/salario/:salario', (req, res) => {
-    procedure.salario(req.params.salario)
-    return res.send('Aumento aplicado!')
-  })
+routes.post(`/registro/funcionario`, (req, res) => {
+  const { nome, cpf, cargo, salario } = req.body
+  procedure.cadastarFuncionario(nome, cpf, cargo, salario)
+  res.send(`Nome: ${req.body.nome}`)
+})
 
 routes.get('/', (req, res) => {
-  res.render('cadastarUser')
+  res.render('cadastrarUsuario')
 })
 
 routes.post('/auth', (req, res) => {
   procedure.cadastarUser(req.body.username, req.body.password)
   procedure.listar_usuarios()
-  res.render('cadastarUser', { });
+  res.render('cadastrarUsuario', { });
 });
 
  routes.get('/demitir', (req, res) => {
    res.render('deletado');
  })
+
 
 routes.post('/excluido', (req, res) => {
   procedure.deletarUser(req.body.nome)
@@ -35,14 +37,17 @@ routes.post('/excluido', (req, res) => {
     // res.send(req.body.nome);
 })
 
-routes.get('/registro', (req, res) => {
+
+// rota para rigistro de funcionários
+routes.get('/sistema', (req, res) => {
  res.render('index')
 })
 
+//consultar registros de funcionários
 routes.post('/registro', (req, res) => {
   console.log(req.body.registro)
   procedure.view_consultar_registro()
-  res.send('Funcionou!')
+  res.send('Funcionou!')  
 })
 
 module.exports = routes;
